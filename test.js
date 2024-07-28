ec818682bc06e3ac5e51e084bc3f14a724dff6ee
Abdul K <prof.asmk@gmail.com>
2024-07-28 15:46:30 +0530

    pay

---
+++ test.js
@@ -0,0 +1,22 @@
+// Function is called, the return value will end up in x
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
+
+//
+function q(x) {
+  return x/(1+x);
+}
+const value = q(2);
+value=9
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+console.log("result",8080);
+
